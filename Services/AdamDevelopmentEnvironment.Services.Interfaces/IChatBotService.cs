dd75431b24e4898b711f3dbbbd0739b3405146e3
vertigra <vertigra.developer@gmail.com>
2023-01-30 22:14:10 +1000

    Added ChatBot interface and service

---
+++ Services/AdamDevelopmentEnvironment.Services.Interfaces/IChatBotService.cs
@@ -0,0 +1,9 @@
+﻿
+namespace AdamDevelopmentEnvironment.Services.Interfaces
+{
+    public interface IChatBotService
+    {
+        string GetMessage(string queryMessage);
+        string GetStatus();
+    }
+}
