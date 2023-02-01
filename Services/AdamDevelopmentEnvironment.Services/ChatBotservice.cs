72b7d94fd711767301d207ae37dbe854cde2ea1f
vertigra <vertigra.developer@gmail.com>
2023-02-01 21:31:13 +1000

    Update

--- Services/AdamDevelopmentEnvironment.Services/ChatBotservice.cs
+++ Services/AdamDevelopmentEnvironment.Services/ChatBotservice.cs
@@ -16,7 +16,7 @@ namespace AdamDevelopmentEnvironment.Services
 
         public async void LoginUserIfNeeded(string phoneNumber)
         {
-            using var client = new WTelegram.Client(21098574, "175a7fe86facf1cacb3c54c63f46f83a");
+            using var client = new WTelegram.Client(,);
             _ = await client.LoginUserIfNeeded();
             _ = await client.Login(phoneNumber);
         }
