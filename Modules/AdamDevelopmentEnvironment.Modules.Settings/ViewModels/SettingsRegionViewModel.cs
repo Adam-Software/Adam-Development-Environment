e4a342e450e41b580c22b24407c45550a7117130
Alexey Nesterov <vertigra.developer@gmail.com>
2023-07-10 15:10:31 +1000

    Added settings window

--- Modules/AdamDevelopmentEnvironment.Modules.Settings/ViewModels/SettingsRegionViewModel.cs
+++ Modules/AdamDevelopmentEnvironment.Modules.Settings/ViewModels/SettingsRegionViewModel.cs
@@ -2,22 +2,40 @@
 using AdamDevelopmentEnvironment.Modules.Settings.Views;
 using AdamDevelopmentEnvironment.Services.Interfaces;
 using HandyControl.Controls;
+using HandyControl.Interactivity;
+using HandyControl.Tools;
+using HandyControl.Tools.Extension;
+using Prism.Commands;
 using Prism.Regions;
 using System;
+using System.Windows;
 
 namespace AdamDevelopmentEnvironment.Modules.Settings.ViewModels
 {
     public class SettingsRegionViewModel : RegionViewModelBase
     {
-        public string Title => "Настройки приложения";
-
+        public DelegateCommand CloseSettingsWindowCommand { get; private set; }
+        public bool IsVisible = false;
+        
         public SettingsRegionViewModel(IRegionManager regionManager, ILoggerService loggerService) : base(regionManager, loggerService)
         {
+           
         }
 
         public override void ConfirmNavigationRequest(NavigationContext navigationContext, Action<bool> continuationCallback)
         {
-            Dialog.Show(new SettingsRegionView());
+            if (IsVisible)
+                return;
+
+            Dialog dialog = Dialog.Show<SettingsRegionView>();
+
+            IsVisible = dialog.IsVisible;
+            dialog.Unloaded += DialogUnloaded;
+        }
+
+        private void DialogUnloaded(object sender, RoutedEventArgs e)
+        {
+            IsVisible = false;
         }
     }
 }
